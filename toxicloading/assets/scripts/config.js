f8c836c6e8702c9d065463eaaf71078292b2e514
kr4yrox <126266327+kr4yrox@users.noreply.github.com>
2023-03-29 23:31:30 +0200

    Add files via upload

---
+++ toxicloading/assets/scripts/config.js
@@ -0,0 +1,30 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "45.61.168.63:30500";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/NHq8bP3TVy"},
+    {name: "instagram", label: "Instagram", description: "An Instagram page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "https://discord.gg/NHq8bP3TVy"},
+    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "https://discord.gg/NHq8bP3TVy"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Toxic #1", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/955831405064114216/1009634881044762644/TOXIC_-_Kopya_2.png"},
+    {name: "Toxic #2", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/955831405064114216/1009634881044762644/TOXIC_-_Kopya_2.png"},
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
